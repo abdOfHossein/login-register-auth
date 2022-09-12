1fd238dae80bba9f908f006c26b4fa159b6792fc
abdOfHossein <92151678+abdOfHossein@users.noreply.github.com>
2022-09-12 14:52:36 +0430

    p12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './config/app/app-config.module';
-import { CommonTypeOrmModuleOptions } from './config/database/common-typeorm';
 import { EmailModule } from './email/email.module';
 import { PackagesModule } from './packages/packages.module';
 import { ProfileModule } from './profile/profile.module';
@@ -14,7 +12,6 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-   
     AppConfigModule,
     UserModule,
     PackagesModule,
